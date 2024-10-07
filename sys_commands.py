712bafdacfb089ac66080cb5ae505fc93a201ccd
Kirill <kirilltymanov2000@gmail.com>
2024-10-07 22:17:30 +0300

    audio commands added

--- sys_commands.py
+++ sys_commands.py
@@ -3,20 +3,43 @@ from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 from comtypes import CLSCTX_ALL
 from ctypes import cast, POINTER
 
-class audio_commands():
-    devices = AudioUtilities.GetSpeakers()  # Получаем основное аудиоустройство (например, динамики или наушники)
-    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-    volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+class audio_controller:
+
+    def __init__(self):
+        self.devices = AudioUtilities.GetSpeakers()  # Получаем основное аудиоустройство (например, динамики или наушники)
+        self.interface = self.devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.volume = cast(self.interface, POINTER(IAudioEndpointVolume))
 
     def get_volume(self):
-        current_volume = audio_commands.volume.GetMasterVolumeLevelScalar()
+        current_volume = self.volume.GetMasterVolumeLevelScalar()
         return current_volume
 
-
-    def set_system_volume(self, level):
+    def set_system_volume(self, value: int):
         # Уровень громкости должен быть от 0.0 до 1.0
-        audio_commands.volume.SetMasterVolumeLevelScalar(level, None)
+        value /= 100
+        self.volume.SetMasterVolumeLevelScalar(value, None)
 
+    def volume_up(self, value: int):
+        value /= 100
+        current_volume = self.get_volume()
+        self.volume.SetMasterVolumeLevelScalar(current_volume + value, None)
 
-    # Пример использования: установить громкость на 50% (0.5)
-    set_system_volume(0.5)
+    def volume_down(self, value: int):
+        value /= 100
+        current_volume = self.get_volume()
+        self.volume.SetMasterVolumeLevelScalar(current_volume - value, None)
+
+    def volume_off(self):
+        self.volume.SetMute(1, None)
+
+    def volume_on(self):
+        self.volume.SetMute(0, None)
+
+if __name__ == "__main__":
+    ac = audio_controller()
+    ac.set_system_volume(40)
+    ac.volume_off()
+    ac.volume_on()
+    ac.volume_up(20)
+    ac.volume_down(40)
