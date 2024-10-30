440a38ee239bf21599baedfcdd66222b77eb0ff0
FunTust <49007663+FunTust@users.noreply.github.com>
2024-10-31 00:49:26 +0300

    small upd voice controller

--- functional/voice_controller.py
+++ functional/voice_controller.py
@@ -24,7 +24,7 @@ class VoiceListening:
         self.p = None
         self.rec = None
         self.vc = VoiceCommands()
-        self.stop_cycle = False
+        self.stop_cycle = True
         self.switch_button_flag = False
         self.keys = {
             "ultimate_key": "моника",
@@ -33,13 +33,14 @@ class VoiceListening:
         }
 
     def start(self):
+        self.stop_cycle = False
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
         self.rec = KaldiRecognizer(self.model, 16000)
         self.listen()
 
     def stop(self):
-        self.stream.stop_stream()
+        self._stop_stream()
         self.stream.close()
         self.p.terminate()
         self.stop_cycle = True
@@ -271,3 +272,4 @@ class VoiceCommands:
 
 if __name__ == '__main__':
     monica = VoiceListening()
+    monica.start()
