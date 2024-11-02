fab1528c1b4d29bbdd960ab1a7333edebd7a8955
Kirill <kirilltymanov2000@gmail.com>
2024-11-02 22:04:12 +0300

    hotkey last fix

--- functional/voice_controller.py
+++ functional/voice_controller.py
@@ -36,11 +36,6 @@ class VoiceListening:
         self.button_thread = None
         self.stop_button_thread = False
         self.op_mod_1_active = False
-        log.debug('создан объект класса VoiceListening')
-        log.critical(self.vc.keys["hold_button"])
-        log.critical(self.vc.keys["hold_button"][0])
-        press_hotkey = keyboard.parse_hotkey(self.vc.keys["hold_button"][0])
-        log.critical(press_hotkey)
 
     def start(self):
         self.stop_cycle = False
@@ -68,8 +63,6 @@ class VoiceListening:
     def listen(self):
         listen_timeout = 5
         last_press_time = None
-        press_hotkey = keyboard.parse_hotkey(self.vc.keys["hold_button"][0])
-
 
         while not self.stop_cycle:
             if self.vc.operating_mode == 0:
@@ -102,13 +95,13 @@ class VoiceListening:
                     time.sleep(0.1)
 
             elif self.vc.operating_mode == 2:
-                if all(keyboard.is_pressed(key) for key in press_hotkey):
+                if keyboard.is_pressed(self.vc.keys["hold_button"][0]):
                     last_press_time = time.time()  # Обновляем время последнего нажатия
                     print("Слушаю")
                     if not self.stream.is_active():
                         self.stream.start_stream()
                         log.debug('запущен поток модели')
-                    while any(keyboard.is_pressed(key) for key in press_hotkey):
+                    while keyboard.is_pressed(self.vc.keys["hold_button"][0]):
                         self.vc.command_recognition(self.read_the_command())
                         last_press_time = time.time()
                 elif last_press_time is not None and (time.time() - last_press_time < listen_timeout):
@@ -120,13 +113,12 @@ class VoiceListening:
                     self.stop_thread()
 
     def check_button(self):
-        switch_hotkey = keyboard.parse_hotkey(self.vc.keys["switch_button"][0])
         while not self.stop_button_thread:
-            if all(keyboard.is_pressed(key) for key in switch_hotkey):
+            if keyboard.is_pressed(self.vc.keys["switch_button"][0]):
                 self.switch_button_flag = not self.switch_button_flag
                 print("Слушаю" if self.switch_button_flag else "Не слушаю")
 
-                while any(keyboard.is_pressed(key) for key in switch_hotkey):
+                while keyboard.is_pressed(self.vc.keys["switch_button"][0]):
                     time.sleep(0.1)
             time.sleep(0.01)
 
